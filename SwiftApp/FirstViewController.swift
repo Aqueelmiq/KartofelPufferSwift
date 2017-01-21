e699f9a96618db257d9d812026e3be871ab5da02
Aqueel Miqdad <amiqdad@hawk.iit.edu>
2017-01-21 07:58:26 -0600

    First Commit

--- SwiftApp/FirstViewController.swift
+++ SwiftApp/FirstViewController.swift
@@ -7,9 +7,16 @@
 //
 
 import UIKit
+import EventKit
 
 class FirstViewController: UIViewController {
+    
+    let eventStore = EKEventStore()
+    var calendar: EKCalendar?
+    var savedEventId:String = ""
 
+    @IBOutlet weak var data: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,92 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onSubmit(_ sender: Any) {
+        
+        let calendar = Calendar.current
+        let date1 = calendar.date(byAdding: .second, value: 5, to: Date())
+        let date2 = calendar.date(byAdding: .second, value: 10, to: Date())
+        
+        if (EKEventStore.authorizationStatus(for: .event) != EKAuthorizationStatus.authorized) {
+            eventStore.requestAccess(to: .event, completion: {
+                granted, error in
+                self.createEvent(eventStore: self.eventStore, title: "DJ's Test Event", startDate: date1!, endDate: date2!)
+            })
+        } else {
+            createEvent(eventStore: self.eventStore, title: data.text!, startDate: date1!, endDate: date2!)
+        }
+        
+        
+    }
+    
+    // Creates an event in the EKEventStore. The method assumes the eventStore is created and
+    // accessible
+    func createEvent(eventStore: EKEventStore, title: String, startDate: Date, endDate: Date) {
+        let event = EKEvent(eventStore: eventStore)
+        
+        event.title = title
+        event.startDate = startDate
+        event.endDate = endDate
+        event.calendar = eventStore.defaultCalendarForNewEvents
+        do {
+            try eventStore.save(event, span: .thisEvent)
+            savedEventId = event.eventIdentifier
+            print(savedEventId)
+        } catch {
+            print("Bad things happened")
+        }
+    }
+    
+    // Removes an event from the EKEventStore. The method assumes the eventStore is created and
+    // accessible
+    func deleteEvent(eventStore: EKEventStore, eventIdentifier: String) {
+        let eventToRemove = eventStore.event(withIdentifier: eventIdentifier)
+        if (eventToRemove != nil) {
+            do {
+                try eventStore.remove(eventToRemove!, span: .thisEvent)
+            } catch {
+                print("Bad things happened")
+            }
+        }
+    }
+    
+    // Responds to button to add event. This checks that we have permission first, before adding the
+    // event
+    func addEvent() {
+        let eventStore = EKEventStore()
+        
+        let startDate =
+            Date()
+        let endDate = startDate.addingTimeInterval(60 * 60) // One hour
+        
+        if (EKEventStore.authorizationStatus(for: .event) != EKAuthorizationStatus.authorized) {
+            eventStore.requestAccess(to: .event, completion: {
+                granted, error in
+                self.createEvent(eventStore: eventStore, title: "DJ's Test Event", startDate: startDate, endDate: endDate)
+            })
+        } else {
+            createEvent(eventStore: eventStore, title: "DJ's Test Event", startDate: startDate, endDate: endDate)
+        }
+    }
+    
+    
+    // Responds to button to remove event. This checks that we have permission first, before removing the
+    // event
+    func removeEvent(sender: UIButton) {
+        let eventStore = EKEventStore()
+        
+        if (EKEventStore.authorizationStatus(for: .event) != EKAuthorizationStatus.authorized) {
+            eventStore.requestAccess(to: .event, completion: { (granted, error) -> Void in
+                self.deleteEvent(eventStore: eventStore, eventIdentifier: self.savedEventId)
+            })
+        } else {
+            deleteEvent(eventStore: eventStore, eventIdentifier: savedEventId)
+        }
+        
+    }
 }
+
+    
+    
+
 
